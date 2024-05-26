2bcfd044ccf01f68f09ea1c2584dbebb27f5baab
yohe <j00968361@gmail.com>
2024-05-26 18:54:00 +0800

    add draw_line

--- yolodetection/car_detection.py
+++ yolodetection/car_detection.py
@@ -1,5 +1,14 @@
 from ultralytics import YOLO
 import cv2
+import torch
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print(f'GPU is available, using {torch.cuda.get_device_name(0)}.')
+else:
+    device = torch.device("cpu")
+    print(f'GPU is unavailable, using cpu instead.')
 
 
 # capture video/ video path
@@ -13,11 +22,12 @@ while True:
     if not ret:
         cap = cv2.VideoCapture("https://cctvn.freeway.gov.tw/abs2mjpg/bmjpg?camera=10000")
         continue
-    results = model.track(frame, persist=True)
+    results = model.track(frame, persist=True, device=device)
+    print(len(results))
     # print(result)
     # cv2.imshow('video', frame)
-    for r in results:
-        print(r.boxes.xywh.numpy())
+    #for r in results:
+        #print(r.boxes)
     
     annotated_frame = results[0].plot()
     cv2.imshow('video', annotated_frame)
